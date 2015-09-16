67a897d072d9dd0bd79714cb594bf3ae4f7a776b
Kelly Becker <kellywerks@gmail.com>
2015-09-16 15:25:01 -0700

    add rental component with hide and show functionality

---
+++ app/components/rental-tile.js
@@ -0,0 +1,12 @@
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+
+    imageHide: function() {
+      this.set('isImageShowing', false)
+    }
+  }
+});
