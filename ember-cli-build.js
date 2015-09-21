c84334145d67cfab9bd926cc1703e0559b103f26
Kelly Becker <kellywerks@gmail.com>
2015-09-21 16:15:46 -0700

    fix typo in build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,8 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-    
+  var app = new EmberApp(defaults, {
+    //Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
