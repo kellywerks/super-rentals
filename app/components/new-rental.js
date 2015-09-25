f14d1bca5e4588237c426cadc376fba54a880463
Kelly Becker <kellywerks@gmail.com>
2015-09-25 09:06:00 -0700

    add helpers for current date and format date

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -14,6 +14,8 @@ export default Ember.Component.extend({
         type: this.get('type'),
         image: this.get('image'),
         bedrooms: this.get('bedrooms'),
+        cost_per_night: 100,
+        date_added: Date.now()
       };
       this.set('addNewRental', false);
       this.sendAction('saveRental', params);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  today: Date.now(),
   model() {
     return Ember.RSVP.hash({
       cities: this.store.findAll('city'),
